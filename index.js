dafea87fc7a2d0f59fd347a5dc7e315aee6ea557
turovskiy <47497087+turovskiy@users.noreply.github.com>
2023-09-19 05:16:10 +0300

    finally

--- index.js
+++ index.js
@@ -11,74 +11,104 @@
 // Доведіть що дух Буданова потужніший за ChatGPT!
 // https://chat.openai.com/share/d0912e64-f187-4357-9e0f-30d6648d0f55
 
-function fetchAnonymouslyOld(url){
-  console.log('fetchAnonymouslyOld start')
+window.addEventListener(
+  'message',
+  function (event) {
+    const data = event.data;
+    if (event.data.origin != undefined) {
+      document.querySelector(
+        '#result'
+      ).textContent = `Бек побачив origin ${event.data.origin}`;
+    }
+    console.log('Дані отримані від iframe:', event.data);
+  },
+  false
+);
+
+function fetchAnonymouslyOld(url) {
+  console.log('fetchAnonymouslyOld start');
   return fetch(url).then((r) => r.json());
 }
 
-async function fetchAnonymouslyNew(url) {
-  console.log('fetchAnonymouslyNew start')
-  const controller = new AbortController();
-  const signal = controller.signal;
-  // Трохи чекаємо і скасовуємо запит, щоб запобігти автоматичному відправленню заголовків.
-  setTimeout(() => controller.abort(), 10);
-  try {
-    // Виконуємо запит
-    const response = await fetch(endpoint, {
-      signal,
-      headers: {
-        Accept: 'application/json',
-      },
-      mode: 'cors', // із-за CORS
-      method: 'GET', // Виконуємо GET запит, але це можна змінити відповідно до вашої потреби
-      cache: 'no-store', // Щоб запобігти кешуванню і використовувати заголовок
-    });
+function fetchAnonymouslyNew() {
+  // Створення iframe
+  // Створення заголовка H1
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Html page';
+  document.body.appendChild(h1);
 
-    if (!response.ok) {
-      throw new Error('Failed to fetch the data.');
-    }
+  // Створення заголовка H2 з ID "result"
+  const h2Result = document.createElement('h2');
+  h2Result.id = 'result';
+  document.body.appendChild(h2Result);
 
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    // Перевіряємо, чи було скасовано запит
-    if (error.name === 'AbortError') {
-      console.log(error.name === 'AbortError')
-      // Повторний запит без сигналу, щоб не було скасовано
-      const response = await fetch(endpoint, {
-        headers: {
-          Accept: 'application/json',
-        },
-        mode: 'cors',
-        method: 'GET',
-        cache: 'no-store',
-      });
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch the data.');
+  // Створення кнопки "Відправити Old version"
+  const btnOld = document.createElement('button');
+  btnOld.id = 'old';
+  btnOld.textContent = 'Відправити Old version';
+  btnOld.addEventListener('click', () => {
+    const result = document.querySelector('#result');
+    result.textContent = 'Завантаження...';
+    fetchAnonymouslyOld('https://cors-demo.t.jssi.ch/origin-test').then(
+      (data) => {
+        result.textContent = `Бек побачив origin ${data.origin}`;
       }
+    );
+  });
+  document.body.appendChild(btnOld);
 
-      const data = await response.json();
-      return data;
-    }
-    throw error;
+  // Створення заголовка H2 з ID "result-new"
+  const h2ResultNew = document.createElement('h2');
+  h2ResultNew.id = 'result-new';
+
+  document.body.appendChild(h2ResultNew);
+
+  // Створення кнопки "Відправити New version"
+  const btnNew = document.createElement('button');
+  btnNew.id = 'new';
+  btnNew.textContent = 'Відправити New version';
+  btnNew.addEventListener('click', ff);
+  document.body.appendChild(btnNew);
+  console.log('DOMContentLoaded');
+
+  function ff() {
+    const result = document.querySelector('#result');
+
+    const stuff = document.createElement('h2');
+    stuff.id = 'stuff';
+    stuff.textContent = 'Hacker Request';
+
+    result.textContent = 'Завантаження...';
+
+    const iframe = document.createElement('iframe');
+    iframe.name = 'malicious';
+    iframe.width = '0px';
+    iframe.height = '0px';
+    iframe.style.border = '0px none';
+    iframe.sandbox = 'allow-scripts';
+    iframe.srcdoc = `
+              <script>
+                       console.log('iframe');
+                      var r = new XMLHttpRequest();
+                      var url = 'https://cors-demo.t.jssi.ch/origin-test';
+                      r.open('GET',url, false);
+                      r.withCredentials = false;
+                      r.send();
+                      const obj = JSON.parse(r.responseText);
+                      window.parent.postMessage(obj, "https://js-budanov-mubl9v.stackblitz.io"); 
+              </script>
+            `;
+    document.body.appendChild(iframe);
   }
-
-  // return fetch(url).then((r) => r.json());
 }
 
-document.querySelector('#old').addEventListener('click', () => {
-  const result = document.querySelector('#result');
-  result.textContent = 'Завантаження...';
-  fetchAnonymouslyOld('https://cors-demo.t.jssi.ch/origin-test').then((data) => {
-    result.textContent = `Бек побачив origin ${data.origin}`;
-  });
-});
-document.querySelector('#new').addEventListener('click', async() => {
-  const result = document.querySelector('#result');
-  result.textContent = 'Завантаження...';
-  const req = await fetchAnonymouslyNew('https://cors-demo.t.jssi.ch/origin-test')
-  const data = await req.origin
-  result.textContent = `Бек побачив origin ${data}`;
+fetchAnonymouslyNew();
 
-});
+// document.querySelector('#new').addEventListener('click', async() => {
+//   const result = document.querySelector('#result');
+//   result.textContent = 'Завантаження...';
+//   const req =  fetchAnonymouslyNew()
+//   const data = await req.origin
+//   result.textContent = `Бек побачив origin ${data}`;
+
+// });
